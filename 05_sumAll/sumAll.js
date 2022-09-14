8f52b3ba0b0423c7ba434ad4e3d03cf840b32f4f
George Ciesinski <george.ciesin@gmail.com>
2022-09-14 15:07:09 -0400

    Finished  exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    console.log(typeof("90"));
+    if ((num1 < 0 || typeof(num1) != "number") || (num2 < 0 || typeof(num2) != "number")) return "ERROR";
 
+    let min;
+    let max;
+    let sum = 0;
+    if (num1 < num2) {
+        min = num1;
+        max = num2;
+    } else {
+        min = num2;
+        max = num1; 
+    }
+    for (min; min<=max; min++) {
+        sum += min;
+    }
+    return sum;
 };
 
 // Do not edit below this line
