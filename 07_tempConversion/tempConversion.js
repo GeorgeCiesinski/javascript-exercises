cb2a9d7e4cea48e3edda6fd291e217d6e4390d02
George Ciesinski <george.ciesin@gmail.com>
2022-09-14 18:28:12 -0400

    Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  const celsius = (farenheit - 32) * (5/9);
+  const celsiusParsed = parseFloat(celsius.toFixed(1));
+  return celsiusParsed;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const farenheit = (celsius * 9 / 5) + 32;
+  const farenheitParsed = parseFloat(farenheit.toFixed(1));
+  return farenheitParsed;
 };
 
 // Do not edit below this line
