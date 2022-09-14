9648313104b89246dbd02344443091344b1a322c
George Ciesinski <george.ciesin@gmail.com>
2022-09-14 14:01:44 -0400

    Completed Lesson 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let reverseArr = [];
+    for (let i = sentence.length - 1; i < sentence.length && i >= 0; i--) {
+        reverseArr.push(sentence[i]);
+    }
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeValues) {
+    for (const value of removeValues) {
+        const index = array.indexOf(value);
+        if (index >= 0) {
+            array.splice(index, 1);
+        }
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
